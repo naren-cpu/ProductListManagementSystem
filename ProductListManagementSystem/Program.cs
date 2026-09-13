86b1d9cf352b5ee405f478e0ab3468bdf53b4452
Narendran-GCP <narendran.anu@gmail.com>
2026-09-13 21:45:28 +0200

    Updated code

--- ProductListManagementSystem/Program.cs
+++ ProductListManagementSystem/Program.cs
@@ -1,76 +1,7 @@
-﻿/*
-Console.WriteLine("-------------------------------");
-Console.WriteLine("LEVEL 1");
-Console.WriteLine("-------------------------------");
-
-
-
-Console.WriteLine("==== PRODUCT LIST APPLICATION ====");
-
-List<Product> products = new List<Product>();
-while (true)
-{
-
-    Console.Write("Enter Category: ");
-    string? category = Console.ReadLine();
-
-    if (category == "q")
-        break;
-
-    Console.Write("Enter Product Name: ");
-    string? productname = Console.ReadLine();
-
-    if (productname == "q")
-        break;
-
-    Console.Write("Enter Price: ");
-    string? priceInput = Console.ReadLine();
-
-    if (priceInput == "q")
-
-    {
-        break;
-    }
-
-
-    decimal.TryParse(priceInput, out decimal price);
-
-    
-
-    Product product1 = new Product();
-
-    product1.Category = category;
-    product1.ProductName = productname;
-    product1.Price = price;
-
-    products.Add(product1);
-
-    Console.WriteLine();
-    Console.WriteLine("Product added successfully!");
-    Console.WriteLine();
-
-    ProductManager manager = new ProductManager();
-}
-
-Console.WriteLine();
-Console.WriteLine("===== PRODUCT LIST =====");
-Console.WriteLine();
-
-
-foreach (Product product in products)
-{
-    Console.WriteLine(
-        $"{product.Category} | {product.ProductName} | {product.Price} kr"
-    );
-}
-
-*/
-
-
-using ProductListManagementSystem;
+﻿using ProductListManagementSystem;
 
 Console.WriteLine("-------------------------------");
-Console.WriteLine("LEVEL 3");
+Console.WriteLine("LEVEL 4");
 Console.WriteLine("-------------------------------");
 
 Console.WriteLine("==== PRODUCT LIST APPLICATION ====");
